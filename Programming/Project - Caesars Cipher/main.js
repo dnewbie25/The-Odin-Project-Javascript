27b8c5f35ded32b3e39e7df11c501aa91623232d
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2021-02-22 22:54:58 -0500

    caesar cipher with DOM

---
+++ Programming/Project - Caesars Cipher/main.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#text');
+const cipher = document.getElementById('cipher');
+const decipher = document.getElementById('decipher');
+const finalMessage = document.querySelector('.result-message');
+const lowerAlpha = 'abcdefghijklmnopqrstuvwxyz';
+const upperAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+// Cipher function
+function cipherMessage(message){
+
+}
+
+// Decipher function
+function decipherMessage(message){
+
+}
